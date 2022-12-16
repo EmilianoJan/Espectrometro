d269862a4b1c0a3c2c2cfcde7a08102e59ebaf52
Emiliano Jan <emilianojan@gmail.com>
2022-12-16 14:34:27 -0300

    Códigos
    Agregado de códigos

--- Codigos/mosaico_colores.m
+++ Codigos/mosaico_colores.m
@@ -1,5 +1,6 @@
-%mosaico de colores
-%genero un mosaico de NxN de distintos colores
+%mosaico_colores.m
+% mosaico de colores
+% genero un mosaico de NxN de distintos colores
 N=3;
 ima=ones(N,N,3);
 ima(:,:,1)=[1 0 0; 1 1 0; 0 0.5 1];
